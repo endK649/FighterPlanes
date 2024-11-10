d599607aedd88fab6062b0c8ae0822faf87ce4fd
ANTRO03 <122835809+ANTRO03@users.noreply.github.com>
2024-11-10 16:12:13 -0500

    Add files via upload

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    public GameObject explosion;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
 
-        if (transform.position.y < -8.5f)
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatIHit)
+    {
+        if (whatIHit.tag == "Player")
         {
+            //I hit the Player!
+            whatIHit.GetComponent<Player>().LoseALife();
+            GameObject.Find("GameManager").GetComponent<GameManager>().Loselife(-1);
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+        else if (whatIHit.tag == "Weapon")
+        {
+            //I am shot!
+            GameObject.Find("GameManager").GetComponent<GameManager>().EarnScore(5);
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(whatIHit.gameObject);
             Destroy(this.gameObject);
         }
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,28 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
     public GameObject player;
-    public GameObject enemy;
+    public GameObject enemyOne;
+    public GameObject cloud;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
+
+    private int score;
+    private int lives;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, Quaternion.identity);
-        InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateEnemyOne", 1f, 3f);
+        CreateSky();
+        score = 0;
+        scoreText.text = "Score: " + score;
+        lives = 3;
+        livesText.text = "Lives: " + lives;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void CreateEnemy()
+    void CreateEnemyOne()
     {
-        Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 9f, 0), Quaternion.identity);
+        Instantiate(enemyOne, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.Euler(0, 0, 180));
     }
+
+    void CreateSky()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloud, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void EarnScore(int newScore)
+    {
+        score = score + newScore;
+        scoreText.text = "Score: " + score;
+
+    }
+    public void Loselife(int newLives)
+    {   lives = lives + newLives;
+        livesText.text = "Lives: " + lives;
+    }
+
 }
